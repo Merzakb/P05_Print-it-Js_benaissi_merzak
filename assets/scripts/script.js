026fc30fd04391ed5d97c8c8e389393b1ac942f3
Merzak Benaissi <merzak89merzak@gmail.com>
2023-09-27 11:47:00 +0200

    add the dots elements

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,3 +10,22 @@ arrowRight.addEventListener("click", () => {
     console.log("arrow right");
 })
 
+/* les points */
+//récupérer la div .dots
+let dotsTag = document.querySelector(".dots")
+
+//une boucle pour créer tous les points
+for (let i = 0; i < slides.length; i++) {
+    //création des élements span dans la div dots
+    let dot = document.createElement("span")
+    //ajouter la class .dot à tous les span
+    dot.classList.add("dot")
+    //condition pour ajouté la class .dot_selected au premier span
+    if (i === 0) {
+        dot.classList.add("dot_selected")
+    }
+   
+    //ajouter les span à la div parent .dots
+    dotsTag.appendChild(dot)
+    
+}
