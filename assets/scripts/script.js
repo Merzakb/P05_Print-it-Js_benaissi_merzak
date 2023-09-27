2b9e539ff963e7e2e3b6281a4edd7e15e4ca3985
Merzak Benaissi <merzak89merzak@gmail.com>
2023-09-27 11:14:57 +0200

    add an eventListener for arrows

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,12 @@
+//varaibles
+let arrowLeft = document.querySelector(".arrow_left")
+let arrowRight = document.querySelector(".arrow_right")
+
+arrowLeft.addEventListener("click", () => {
+    console.log("arrow left");
+})
+
+arrowRight.addEventListener("click", () => {
+    console.log("arrow right");
+})
 
